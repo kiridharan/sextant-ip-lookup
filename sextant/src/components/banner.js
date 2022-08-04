da6bb5d441353baddf2c908fef23906162bf2fab
kiridharan <kiridharan217dec2001@gmail.com>
2022-08-04 12:22:29 +0530

    Task 2

---
+++ sextant/src/components/banner.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './banner.css';
+class Banner extends Component {
+    render() {
+        return (
+            <div className="Banner">
+                <h1>{this.props.bannerText}</h1>
+            </div>
+        );
+    }
+}
+
+export default Banner;
